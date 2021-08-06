dc0193fb9b6de5a365c7f4335b60b186af569b70
GuRuGu <peter.k.nets@gmail.com>
2021-08-06 11:16:07 +0300

    Add code for Logic handles a valid puzzle string of 81 characters

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,22 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    const regExp = new RegExp(/^(\d?|\.?)+$/);
+    const testResults = regExp.test(puzzleString);
+
+    if (testResults) {
+      return "All is good";
+    } else {
+      return "Not good";
+    }
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,17 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
+const { puzzlesAndSolutions } = require("../controllers/puzzle-strings");
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
 
-suite('UnitTests', () => {
+suite("UnitTests", () => {
+  test("Logic handles a valid puzzle string of 81 characters", done => {
+    const [puzzleString] = puzzlesAndSolutions[0];
 
+    const result = solver.validate(puzzleString);
+
+    assert.equal(result, "All is good");
+    done();
+  });
 });
