f9fa40b5e234a8cc21c0ac96df72aa7231f403eb
GuRuGu <peter.k.nets@gmail.com>
2021-10-08 18:40:33 +0300

    Add test for saveEmptyPositions method

--- tests/3_new-tests.js
+++ tests/3_new-tests.js
@@ -7,6 +7,7 @@ const solver = new Solver();
 const board =
   "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 let parsedBoard;
+let emptyPositions;
 
 suite("Sudoku Solver", () => {
   suite("#parseBoard()", () => {
@@ -27,6 +28,61 @@ suite("Sudoku Solver", () => {
       expect(parsedBoard.length).to.equal(9);
       expect(parsedBoard[0].length).to.equal(9);
       expect(parsedBoard).to.eql(expectedBoard);
+    });
+  });
+
+  suite("#saveEmptyPositions()", () => {
+    test("should collect positions of all empty cells", () => {
+      emptyPositions = solver.saveEmptyPositions(parsedBoard);
+
+      const expectedEmptyPositions = [
+        [0, 1],
+        [0, 3],
+        [0, 4],
+        [0, 6],
+        [1, 0],
+        [1, 1],
+        [1, 4],
+        [1, 7],
+        [2, 0],
+        [2, 2],
+        [2, 3],
+        [2, 5],
+        [2, 6],
+        [2, 7],
+        [2, 8],
+        [3, 0],
+        [3, 2],
+        [3, 3],
+        [3, 5],
+        [3, 6],
+        [3, 7],
+        [3, 8],
+        [4, 1],
+        [4, 3],
+        [4, 8],
+        [5, 1],
+        [5, 3],
+        [5, 5],
+        [5, 6],
+        [5, 8],
+        [6, 2],
+        [6, 3],
+        [6, 4],
+        [6, 6],
+        [6, 7],
+        [7, 0],
+        [7, 1],
+        [7, 4],
+        [7, 5],
+        [7, 6],
+        [7, 7],
+        [8, 5],
+        [8, 8],
+      ];
+
+      expect(emptyPositions.length).to.equal(43);
+      expect(emptyPositions).to.eql(expectedEmptyPositions);
     });
   });
 });
