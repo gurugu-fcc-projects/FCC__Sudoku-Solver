40a15f4e05a0913a13168bc844ff7630b1ea5fb1
GuRuGu <peter.k.nets@gmail.com>
2021-12-29 14:18:25 +0200

    Add some final tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -231,6 +231,23 @@ suite("Functional Tests", () => {
     done();
   });
 
+  test("Check a puzzle placement with invalid placement coordinate: POST request to /api/check", (done) => {
+    const puzzle =
+      "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37";
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle, coordinate: "a111", value: "7" })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "error");
+        assert.equal(res.body.error, "Invalid coordinate");
+      });
+
+    done();
+  });
+
   test("Check a puzzle placement with correct value already placed: POST request to /api/check", (done) => {
     const puzzle =
       "7.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
@@ -248,7 +265,7 @@ suite("Functional Tests", () => {
     done();
   });
 
-  test("Check a puzzle placement with an invalid value: POST request to /api/check", (done) => {
+  test("Check a puzzle placement with an invalid placement value: POST request to /api/check", (done) => {
     const puzzle =
       "7.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 
